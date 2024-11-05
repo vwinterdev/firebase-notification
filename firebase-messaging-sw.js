b950842343939a7382e18ad5cf710cbd96a60559
Akulllvlllad <Akulllvlllad@gmail.com>
2024-11-05 17:25:55 +0700

    save

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,34 @@
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCKcRQNdPtVIdYy-ZYLK0PvcYHhm85LRJk",
+    authDomain: "notification-31228.firebaseapp.com",
+    projectId: "notification-31228",
+    storageBucket: "notification-31228.firebasestorage.app",
+    messagingSenderId: "515089145210",
+    appId: "1:515089145210:web:11ef75510e5735edc28ac3",
+    measurementId: "G-4QVBB372GX",
+    vapidKey: 'BB9BiqPfKEWleyHgX8MnXSk6PGUV5m0ltzA58OFMYXKrpjK9MKx5MqLbxabjBB6qJY4Q9H_2Jdsc-z4I-n9uAnI'
+};
+  
+firebase.initializeApp(firebaseConfig);
+firebase.messaging();
+
+self.addEventListener('push', (event) => {
+    console.log('Push событие получено:', event);
+    if (event.data) {
+        const data = event.data.json();
+        const notificationTitle = data.notification.title;
+        const notificationOptions = {
+        body: data.notification.body,
+        icon: '/favicon.ico',
+        };
+
+        // Показ уведомления
+        console.log(self.registration.showNotification)
+        self.registration.showNotification(notificationTitle, notificationOptions);
+    }
+
+});
+  
